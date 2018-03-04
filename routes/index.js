771f4f769db8a156ba043c2d9bb33b857f088dba
OlegBalabukh <olegukoff@gmail.com>
2018-03-04 23:28:20 +0200

    remove else 3 modules

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {  // we don't need next (callback) in this challange
+  res.render('index', { title: 'Express', name: 'Oleg' });
 });
 
 module.exports = router;
